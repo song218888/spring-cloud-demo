24c37cc9d87192a90f3011d74aba1ec07c883071
song218888 <522658066@qq.com>
2018-03-29 18:13:29 +0800

    test

--- spring-cloud-demo-01-eureka/src/main/java/com/tiancheng/EurekaServer.java
+++ spring-cloud-demo-01-eureka/src/main/java/com/tiancheng/EurekaServer.java
@@ -11,5 +11,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaServer {
 	public static void main(String[] args) {
 		new SpringApplication(EurekaServer.class).run(args);
+		System.out.println("test");
 	}
 }
